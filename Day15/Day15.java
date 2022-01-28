1d11ec5c0e7019be0c39efefdad3db70ac5f9071
DarkoBashukoski <darko_bashukoski@outlook.com>
2022-01-28 12:49:35 +0100

    Fixed Warnings

--- Day15/Day15.java
+++ Day15/Day15.java
@@ -3,7 +3,6 @@ package Day15;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
